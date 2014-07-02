2ae4ce793cad00125098238310f36c96beb23098
mz121star <mz121star@gmail.com>
2014-07-02 12:09:17 +0800

    div

--- addshop.php
+++ addshop.php
@@ -15,17 +15,34 @@ if (count($_POST)) {
 }
 $shops = $collection->find();
 ?>
+
+
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <style type="text/css">
+                #allmap{width:400px;height:400px;}
+    </style>
+    <script type="text/javascript" name="baidu-tc-cerfication" src="http://apps.bdimg.com/cloudaapi/lightapp.js#8994ab4df9d6a5a169421089618fc5c3"></script><script type="text/javascript">window.bd && bd._qdc && bd._qdc.init({app_id: '2960154a5bbf7ef967f95eff'});</script>
+    <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=lcO3zSdb4cgCduHNBT3AoAR9"></script>
+    <title>获取商家坐标</title>
+</head>
+<body>
+<div id="allmap"></div>
 <form method="post" action="addshop.php" enctype="multipart/form-data">
     <table>
-        
+
         <tr><td>餐厅名称</td><td><input name="name" type="text"></td></tr>
         <tr><td>星级</td><td><input name="star" type="text"></td></tr>
-        <tr><td>位置</td><td><input name="location" type="text"></td></tr>
+        <tr><td>地址</td><td><input name="location" type="text"></td></tr>
+        <tr><td>坐标</td><td><input name="geo" type="text"></td></tr>
         <tr><td>平均价格</td><td><input name="price" type="text"></td></tr>
         <tr><td>图片</td><td><input name="image" type="file"></td></tr>
         <tr><td colspan="2"><input type="submit" value="add"></td></tr>
     </table>
-    
+
 </form>
 
  <?php
@@ -50,3 +67,18 @@ $shops = $collection->find();
     <?php } }
     ?>
 </table>
+
+</body>
+</html>
+<script type="text/javascript">
+
+    // 百度地图API功能
+    var map = new BMap.Map("allmap");            // 创建Map实例
+    map.centerAndZoom("大连",12);                   // 初始化地图,设置城市和地图级别。
+
+    map.addEventListener("click",function(e){
+        var marker1 = new BMap.Marker(new BMap.Point(e.point.lng,e.point.lat));  // 创建标注
+        map.addOverlay(marker1);              // 将标注添加到地图中
+     //   alert(e.point.lng + "," + e.point.lat);
+    });
+</script>

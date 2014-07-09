1477925c29bf90c04092592bfbf7d92d80533053
mz121star <mz121star@gmail.com>
2014-07-09 11:15:00 +0800

    aa

--- ticket.php
+++ ticket.php
@@ -74,8 +74,10 @@ $('.proContent  img').attr('height',"100%");
         <div class="brr">
              <?php if(!$fav){?>
             <a id="addstar" href="javascript:;"  ><i   class="fa fa-star-o"></i> </a>
+               <a id="removestar" style="display: none" href="javascript:;"  ><i   class="fa fa-star"></i> </a>
             <?php }else{ ?>
-            <a id="removestar"   href="javascript:;"  ><i   class="fa fa-star"></i> </a>
+                 <a id="addstar" href="javascript:;"  style="display: none"  ><i   class="fa fa-star-o"></i> </a>
+                 <a id="removestar" href="javascript:;"  ><i   class="fa fa-star"></i> </a>
             <?php } ?>
         </div>
     </div>
